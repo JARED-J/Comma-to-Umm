70ce51629e46205ef53ba0e5ecba3d27fc2a0bfa
Jared E Jackson <jaredjxn15@gmail.com>
2018-03-06 13:52:19 -0500

    changed reqex, removed bad joke

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 walk(document.body);
 
 function walk(node){
-	// Through extensive research I had found that a regex is probably not the best way to go for looping through heavy webpages
-    // So stack overflow to the rescue.
 	// http://is.gd/mwZp7E
 	var child, next;
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
-
 	switch ( node.nodeType ) {
 		case 1:  // Element
 		case 9:  // Document
@@ -28,14 +25,18 @@ function walk(node){
 }
 
 function handleText(textNode) {
-	var v = textNode.nodeValue;
+	let replaced = textNode.nodeValue;
 
-	// Originally for one case, ex: changing every instance of a single word,
-	// but for this implementation it needed variety, and who dosen't like the spice of life?
+	// Originally for one case, ex: changing every instance of a single word
+	// added number generator to enhance variety, compared to just repeating umm, to give it a more natural feel.
     if(Math.round(Math.random(1))) {
-        v = v.replace(/\b,\b/g, "ummm");
+        replaced = replaced.replace(/,/ig, "ummm");
     } else {
-        v = v.replace(/\b,\b/g, "and uhhh");
-    }
+        replaced = replaced.replace(/,/ig, "and uhhh");
+	}
+	// old regex
+	// replaced = replaced.replace(/\b,\b/g, "ummm")
+	// new regex
+	// /,/ig
 	textNode.nodeValue = v;
 }
